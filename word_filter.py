48dabdd3d8ad7166de10affab6b3383cb7e54c03
chikara <chikara.f.yoshida@gmail.com>
2018-11-21 15:20:20 +0900

    複数の入力されたNGワードに対応できるようにした。

--- word_filter.py
+++ word_filter.py
@@ -3,10 +3,20 @@ class WordFilter:
         self.search = filter1
 
     def detect(self, sentence, censored):
-        if self.search in sentence:
-            sentence = sentence.replace('アーセナル', censored)
+        for i in range(0, len(self.search)):
+            if self.search[i] in sentence:
+                sentence = sentence.replace(self.search[i], censored)
         return sentence
 
-my_filter = WordFilter("アーセナル")
+
+fil = []
+counter = 1
+while True:
+    ng_word = input('NGワード：' + str(counter) + ':')
+    if ng_word == 'e':
+        break
+    fil.append(ng_word)
+    counter += 1
+my_filter = WordFilter(fil)
 print(my_filter.detect('昨日のアーセナルの試合は熱かった', 'ピー'))
 print(my_filter.detect('昨日のリバプールの試合は熱かった', 'ピー'))
