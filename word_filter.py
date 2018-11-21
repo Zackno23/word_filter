740f013f694bc607e26ae37b10f908f4c8e0c9be
chikara <chikara.f.yoshida@gmail.com>
2018-11-21 18:57:35 +0900

    フィルターを変更する昨日を実装

--- word_filter.py
+++ word_filter.py
@@ -12,26 +12,43 @@ class WordFilter:
 print("NGワードを設定してください")
 print("NGワードを入力し終わったら、半角でeを入力してください")
 censored = input('伏せ字を設定してください：')
+fil = []
 
 
-def ng_word_list():  # NGワードの設定
-    fil = []
+def ng_word_list(filter_list):  # NGワードの設定
     counter = 1
     while True:
+        global fil
         ng_word = input('NGワード：' + str(counter) + ':')
         if ng_word == 'e':
             break
-        fil.append(ng_word)
+        filter_list.append(ng_word)
         counter += 1
-    return fil
+        fil = filter_list
+    return filter_list
 
 
 #repeat == yである限り、フィルタリングを繰り返せる。
 repeat = "y"
 while repeat.lower() == 'y':
-    my_filter = WordFilter(ng_word_list())
-    print(my_filter.detect('昨日のアーセナルの試合は熱かった', censored))
-    print(my_filter.detect('昨日のリバプールの試合は熱かった', censored))
-    repeat = input('フィルタリングをし直しますか?(y/n)')
-    if repeat.lower() == 'n':
-        break
+    my_filter = WordFilter(ng_word_list(fil))
+    # fil = ng_word_list(fil)
+    while True:
+        print(my_filter.detect('昨日のアーセナルの試合は熱かった', censored))
+        print(my_filter.detect('昨日のリバプールの試合は熱かった', censored))
+        repeat = input('フィルタリングをし直しますか?(y/n)')
+        if repeat.lower() == 'n':
+            break
+        else:
+            while True:
+                for i in range(0, len(fil)):
+                    print(i + 1, fil[i], '', end="")
+                change_NG_Number = int(input('どのNGワードを変更しますか?'))
+                if change_NG_Number < 1 or change_NG_Number > len(fil):
+                    print("正しい数値を入力してください")
+                    continue
+
+                change_NG_Word = input('新しいNGワード')
+                fil[change_NG_Number - 1] = change_NG_Word
+                break
+            continue
