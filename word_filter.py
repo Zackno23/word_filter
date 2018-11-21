8074377a9e79ec1743e0f32cfcc876f134ded9a9
chikara <chikara.f.yoshida@gmail.com>
2018-11-21 14:54:14 +0900

    <censored> をピーに変更

--- word_filter.py
+++ word_filter.py
@@ -2,12 +2,11 @@ class WordFilter:
     def __init__(self,filter1):
         self.search = filter1
 
-    def detect(self, sentence):
+    def detect(self, sentence, censored):
         if self.search in sentence:
-            sentence = sentence.replace('アーセナル', '<censored>')
+            sentence = sentence.replace('アーセナル', censored)
         return sentence
 
-
 my_filter = WordFilter("アーセナル")
-print(my_filter.detect('昨日のアーセナルの試合は熱かった'))
-print(my_filter.detect('昨日のリバプールの試合は熱かった'))
+print(my_filter.detect('昨日のアーセナルの試合は熱かった', 'ピー'))
+print(my_filter.detect('昨日のリバプールの試合は熱かった', 'ピー'))
